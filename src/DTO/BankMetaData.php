b2a4f2560a5d8c29aa562887d27753defb05dbb8
Lenka Kolarova <lenka.kolarova@unnits.com>
2023-06-07 16:07:29 +0200

    Add BankMetaData DTO #14

---
+++ src/DTO/BankMetaData.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unnits\BankId\DTO;
+
+class BankMetaData
+{
+    public function __construct(
+        public readonly string $id,
+        public readonly string $name,
+        public ?string $companyIdentificationNumber = null,
+        public ?string $verificationDate = null,
+    ) {
+        //
+    }
+
+    /**
+     * @return array{'id': string, 'name': string, 'companyIdentificationNumber': string|null, 'verificationDate': string|null}
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'companyIdentificationNumber' => $this->companyIdentificationNumber,
+            'verificationDate' => $this->verificationDate,
+        ];
+    }
+
+    /**
+     * @param array{'id': string, 'name': string, 'verificationClaim': string|null, 'verificationDate': string|null} $data
+     * @return self
+     */
+    public static function create(array $data): self
+    {
+        return new self(
+            id: $data['id'],
+            name: $data['name'],
+            companyIdentificationNumber: $data['verificationClaim'] ?? null,
+            verificationDate: $data['verificationDate'] ?? null,
+        );
+    }
+}
